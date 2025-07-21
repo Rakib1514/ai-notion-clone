f8e51445f62776693ecf9a244e6f64f48a479124
Rakib1514 <rkrakib1514@outlook.com>
2025-07-22 00:53:52 +0600

    initialize new server cloudlflare; installed openAI, HONO

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import OpenAI from 'openai';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+
+type Bindings = {
+	OPEN_AI_KEY: string;
+	AI: Ai;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+	'/*',
+	cors({
+		origin: '*',
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
+		allowMethods: ['GET', 'POST', 'PUT', 'OPTIONS'],
+		exposeHeaders: ['X-Kuma-Revision', 'Content-Length'],
+		maxAge: 600,
+		credentials: true,
+	})
+);
